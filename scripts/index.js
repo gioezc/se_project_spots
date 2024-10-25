07953c2b401faa1417f6432f9c78746b8d65d0db
Brian Ezcurra <brianezcurra@gmail.com>
2024-10-25 16:07:30 -0400

    Update blocks/cards.css blocks/content.css index.html scripts/index.js

--- scripts/index.js
+++ scripts/index.js
@@ -35,6 +35,24 @@ const editModalCloseBtn = editModal.querySelector(".modal__close-btn");
 const nameInput = editModal.querySelector("#profile-name-input");
 const jobInput = editModal.querySelector("#profile-description-input");
 
+const cardTemplate = document.querySelector("#card-template");
+const cardList = document.querySelector(".cards__list");
+
+function getCardElement(data) {
+  const cardElement = cardTemplate.content
+    .querySelector(".card")
+    .cloneNode(true);
+
+  const cardNameEl = cardElement.querySelector(".card__title");
+  const cardImageEl = cardElement.querySelector(".card__image");
+
+  cardNameEl.textContent = data.name;
+  cardImageEl.setAttribute("src", data.link);
+  cardImageEl.setAttribute("alt", data.name);
+
+  return cardElement;
+}
+
 function openModal() {
   nameInput.value = profileNameElement.textContent;
   jobInput.value = profileJobElement.textContent;
@@ -56,9 +74,7 @@ profileEditButton.addEventListener("click", openModal);
 editModalCloseBtn.addEventListener("click", closeModal);
 editFormElement.addEventListener("submit", handleProfileFormSubmit);
 
-// editProfileModal
-//   .querySelector("#name")
-//   .setAttribute("value", profileNameElement);
-// editProfileModal
-//   .querySelector("#description")
-//   .setAttribute("value", profileJobElement);
+for (let i = 0; i < initialCards.length; i++) {
+  const cardElement = getCardElement(initialCards[i]);
+  cardList.prepend(cardElement);
+}
